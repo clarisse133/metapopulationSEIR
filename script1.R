a1a10fdee4a6bc7dc94c08e782273a7039105b1d
clarisse <clarissemoura2205@gmail.com>
2025-09-08 17:56:43 -0300

    creating the basic function

---
+++ script1.R
@@ -0,0 +1,96 @@
+
+#----packages----
+library(deSolve)
+library(devtools)
+library(ggplot2)
+
+#' @param betta transmission rate
+#' @param gamma recovery rate
+#' @param un birth rate
+#' @param um mortality rate
+#' @param M migration rate
+#' @param sigma transition rate from exposed to infected
+#' 
+#' @export
+
+#--- Ma = Mb = M ---
+#--- un = um ---
+
+#---function---
+
+metap <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+  
+  dSa = -betta*Sa*Ia + Sa*(un - um) + M*(Sb - Sa)
+  dEa = betta*Sa*Ia - Ea*(um + sigma) + M*(Eb - Ea)
+  dIa = sigma*Ea - Ia*(um + gamma) + M*(Ib - Ia)
+  dRa = gamma*Ia - Ra*um + M*(Rb- Ra)
+     
+  dSb = -betta*Sb*Ib + Sb*(un - um) + M*(Sa - Sb)
+  dEb = betta*Sb*Ib - Eb*(um + sigma) + M*(Ea - Eb)
+  dIb = sigma*Eb - Ib*(um + gamma) + M*(Ia - Ib)
+  dRb = gamma*Ib - Rb*um + M*(Ra- Rb)
+  
+   return(list(c(dSa, dEa, dIa, dRa, dSb, dEb, dIb, dRb)))
+
+
+  })
+}
+#---parameter values---
+
+parameters <- c(
+  betta=0.4,
+  gamma=0.1,
+  un=0.02,
+  um=0.02,
+  M=0.4,
+  sigma=0.8
+) 
+
+#---initial conditions---
+
+state <- c(
+  Sa=0.9,
+  Ea=0,
+  Ia=0.1,
+  Ra=0,
+  
+  Sb=0.9,
+  Eb=0,
+  Ib=0.1,
+  Rb=0
+  
+)
+
+time <- seq(0, 100, by=1)
+
+#--- solve the EDO ---
+
+output <- ode(y = state, times = time, func = metap, parms = parameters)
+head(output)
+
+#---plot A---
+
+out_df <- as.data.frame(output)
+
+ggplot(out_df, aes(x = time)) +
+  geom_line(size = 0.7, aes(y = Sa, color = "Susceptible A")) +
+  geom_line(size = 0.7, aes(y = Ea, color = "Exposed A")) +
+  geom_line(size = 0.7, aes(y = Ia, color = "Infected A")) +
+  geom_line(size = 0.7, aes(y = Ra, color = "Recovered A")) +
+  labs(x = "Time(days)", y = "Proportion of population", title = "SEIR Dynamics for the population A") +
+  scale_color_manual(values = c("Susceptible A" = "blue", "Exposed A" = "orange", "Infected A" = "red", "Recovered A" = "green")) +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+#---plot B---
+
+ggplot(out_df, aes(x = time)) +
+  geom_line(size = 0.7, aes(y = Sb, color = "Susceptible B")) +
+  geom_line(size = 0.7, aes(y = Eb, color = "Exposed B")) +
+  geom_line(size = 0.7, aes(y = Ib, color = "Infected B")) +
+  geom_line(size = 0.7, aes(y = Rb, color = "Recovered B")) +
+  labs(x = "Time(days)", y = "Proportion of population", title = "SEIR Dynamics for the population B") +
+  scale_color_manual(values = c("Susceptible B" = "blue", "Exposed B" = "orange", "Infected B" = "red", "Recovered B" = "green")) +
+  theme_minimal() +
+  theme(legend.title = element_blank())
